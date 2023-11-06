5dc3d2e999b822ffb5c52c8f65b88451dadb6229
Ju Young Yang <106505763+JuYoung-Yang-00@users.noreply.github.com>
2023-11-06 10:46:08 -0500

    add dataset, valid jsonl

---
+++ valid.py
@@ -0,0 +1,17 @@
+import json
+
+def validate_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        for i, line in enumerate(file, 1):
+            try:
+                json.loads(line)
+            except json.JSONDecodeError as e:
+                print(f"Invalid JSON on line {i}: {e}")
+                return False
+    return True
+
+is_valid = validate_jsonl('./data.jsonl')
+if is_valid:
+    print("The JSONL file is valid.")
+else:
+    print("The JSONL file has issues.")
